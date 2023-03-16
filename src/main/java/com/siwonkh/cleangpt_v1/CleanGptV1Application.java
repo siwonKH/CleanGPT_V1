42096d22a7fe3c61c49118cf692641b4c616604d
siwonKH <kanghyunkim313@gmail.com>
2023-03-16 09:55:05 +0900

    Initial commit

---
+++ src/main/java/com/siwonkh/cleangpt_v1/CleanGptV1Application.java
@@ -0,0 +1,13 @@
+package com.siwonkh.cleangpt_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanGptV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanGptV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/siwonkh/cleangpt_v1/CleanGptV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siwonkh.cleangpt_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanGptV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
