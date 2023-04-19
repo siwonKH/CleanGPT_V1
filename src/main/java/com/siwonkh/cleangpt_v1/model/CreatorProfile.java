04e81807e993bd41a982b12f6f73fec4c0b5cca0
siwonKH <kanghyunkim313@gmail.com>
2023-04-19 12:13:13 +0900

    ADD CreatorProfile

---
+++ src/main/java/com/siwonkh/cleangpt_v1/model/CreatorProfile.java
@@ -0,0 +1,37 @@
+package com.siwonkh.cleangpt_v1.model;
+
+public class CreatorProfile {
+    private String title;
+    private String description;
+    private String url;
+
+    public CreatorProfile(String title, String description, String url) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
