dcdcfadba96244f8995d2f6d8cec66dbe6f6d6e9
siwonKH <kanghyunkim313@gmail.com>
2023-04-20 18:05:17 +0900

    UPDATE HomeController

---
+++ src/main/java/com/siwonkh/cleangpt_v1/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.siwonkh.cleangpt_v1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/comment")
+    public String comments(Model model) {
+        return "searchVideoComments";
+    }
+}
