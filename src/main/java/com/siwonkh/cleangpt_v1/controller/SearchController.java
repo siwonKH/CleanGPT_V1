b58ba128536bc31825b01c1aeec07417bd8473cf
siwonKH <kanghyunkim313@gmail.com>
2023-04-20 18:08:24 +0900

    ADD CreatorVideo feature

--- src/main/java/com/siwonkh/cleangpt_v1/controller/SearchController.java
+++ src/main/java/com/siwonkh/cleangpt_v1/controller/SearchController.java
@@ -1,9 +1,11 @@
 package com.siwonkh.cleangpt_v1.controller;
 
+import com.siwonkh.cleangpt_v1.model.CreatorVideo;
 import com.siwonkh.cleangpt_v1.model.VideoComment;
 import com.siwonkh.cleangpt_v1.model.CreatorProfile;
 import com.siwonkh.cleangpt_v1.util.SearchCommentThread;
 import com.siwonkh.cleangpt_v1.util.SearchCreatorProfile;
+import com.siwonkh.cleangpt_v1.util.SearchCreatorVideo;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
@@ -43,13 +45,19 @@ public class SearchController {
             JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("snippet").getJSONObject("thumbnails").getJSONObject("default");
             searchedImages.add(jsonObject.getString("url"));
         }
+        List<String> searchedChannelIds = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("id");
+            searchedChannelIds.add(jsonObject.getString("channelId"));
+        }
 
         List<CreatorProfile> creatorProfiles = new ArrayList<>();
         for (int i = 0; i < searchedTitles.size(); i++) {
             String title = searchedTitles.get(i);
             String desc = searchedDesc.get(i);
             String url = searchedImages.get(i);
-            CreatorProfile creatorProfile = new CreatorProfile(title, desc, url);
+            String channelId = searchedChannelIds.get(i);
+            CreatorProfile creatorProfile = new CreatorProfile(title, desc, url, channelId);
             creatorProfiles.add(creatorProfile);
         }
         model.addAttribute("profiles", creatorProfiles);
@@ -61,8 +69,6 @@ public class SearchController {
         SearchCommentThread searchCommentThread = new SearchCommentThread(APIKey);
         searchCommentThread.setVideo(video);
         JSONObject comments =  searchCommentThread.getApiResponse();
-
-        System.out.println(comments);
 
         JSONArray jsonArray = comments.getJSONArray("items");
         List<String> searchedComments = new ArrayList<>();
@@ -91,5 +97,52 @@ public class SearchController {
         }
         model.addAttribute("comments", videoComments);
         return "search/videoComments";
+    }
+
+    @GetMapping("search/video")
+    public String searchCreatorVideos(Model model, @RequestParam("channelId") String channelId) throws Exception {
+        SearchCreatorVideo searchCreatorVideo = new SearchCreatorVideo(APIKey);
+        searchCreatorVideo.setChannelId(channelId);
+        JSONObject videos = searchCreatorVideo.getApiResponse();
+
+        JSONArray jsonArray = videos.getJSONArray("items");
+        List<String> searchedVideoIds = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("id");
+            searchedVideoIds.add(jsonObject.getString("videoId"));
+        }
+        List<String> searchedTitles = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("snippet");
+            searchedTitles.add(jsonObject.getString("title"));
+        }
+        List<String> searchedDescription = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("snippet");
+            searchedDescription.add(jsonObject.getString("description"));
+        }
+        List<String> searchedThumbnails = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("snippet").getJSONObject("thumbnails").getJSONObject("medium");
+            searchedThumbnails.add(jsonObject.getString("url"));
+        }
+        List<String> searchedPublishedAt = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("snippet");
+            searchedPublishedAt.add(jsonObject.getString("publishedAt"));
+        }
+
+        List<CreatorVideo> creatorVideos = new ArrayList<>();
+        for (int i = 0; i < searchedTitles.size(); i++) {
+            String videoIds = searchedVideoIds.get(i);
+            String title = searchedTitles.get(i);
+            String description = searchedDescription.get(i);
+            String thumbnail = searchedThumbnails.get(i);
+            String publishedAt = searchedPublishedAt.get(i);
+            CreatorVideo creatorVideo = new CreatorVideo(videoIds, title, description, thumbnail, publishedAt);
+            creatorVideos.add(creatorVideo);
+        }
+        model.addAttribute("videos", creatorVideos);
+        return "search/creatorVideos";
     }
 }
--- src/main/java/com/siwonkh/cleangpt_v1/util/SearchCreatorVideo.java
+++ src/main/java/com/siwonkh/cleangpt_v1/util/SearchCreatorVideo.java
@@ -1,2 +1,51 @@
-package com.siwonkh.cleangpt_v1.util;public class SearchCreatorVideo {
+package com.siwonkh.cleangpt_v1.util;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class SearchCreatorVideo {
+    private final String BASE_URL = "https://www.googleapis.com/youtube/v3/search";
+    private final String APIKey;
+    private String url;
+
+    public SearchCreatorVideo(String APIKey) {
+        this.APIKey = APIKey;
+    }
+
+    public void setChannelId(String channelId) {
+        String urlStr = BASE_URL + "?part=snippet&type=video";
+        urlStr += "&key=" + APIKey;
+        urlStr += "&channelId=" + channelId;
+        urlStr += "&maxResults=" + "50";
+        urlStr += "&order=" + "date";
+        this.url = urlStr;
+    }
+
+    public JSONObject getApiResponse() throws Exception {
+        URL apiUrl = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/json");
+
+        if (connection.getResponseCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((connection.getInputStream()), StandardCharsets.UTF_8));
+        String output;
+        StringBuilder apiResponse = new StringBuilder();
+
+        while ((output = bufferedReader.readLine()) != null) {
+            apiResponse.append(output);
+        }
+
+        connection.disconnect();
+
+        return new JSONObject(apiResponse.toString());
+    }
 }
